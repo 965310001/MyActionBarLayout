ceff688d25b71fba8810b6118b68edaf2bf89c94
fly wei <hnyzwtf@163.com>
2015-12-18 16:54:17 +0800

    second commit 20151218

--- src/com/example/myactionbar/MainActivity.java
+++ src/com/example/myactionbar/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -13,6 +14,7 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
 		MyActionBar actionBar = (MyActionBar)findViewById(R.id.actionbar);
 		actionBar.setOnMyActionbarClickListener(new MyActionbarClickListener() {
--- src/com/example/myactionbar/MyActionBar.java
+++ src/com/example/myactionbar/MyActionBar.java
@@ -12,8 +12,9 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 public class MyActionBar extends RelativeLayout {
+	//声明我们需要的控件，这些控件是和attrs.xml中我们定义的属性对应的
 	private Button leftButton, rightButton;
-	private TextView tvTitle;
+	private TextView tvTitle;//actionbar控件上显示的标题
 	
 	private int leftTextColor;
 	private Drawable leftBackground;
@@ -24,20 +25,21 @@ public class MyActionBar extends RelativeLayout {
 	private String rightText;
 	
 	private String title;
-	private int titleTextColor;
-	private float titleTextSize;
+	private int titleTextColor;//显示文字的颜色
+	private float titleTextSize;//显示出来的文字的大小
 	
 	private Drawable actionBarBackground;
 	
 	private LayoutParams leftParams, rightParams, titleParams;//设置控件的布局
-	private MyActionbarClickListener listener;
+	private MyActionbarClickListener listener;//定义接口对象
 	
-	public interface MyActionbarClickListener
+	public interface MyActionbarClickListener//自定义接口
 	{
 		void leftClick();
 		void rightClick();
 	}	
-	public void setOnMyActionbarClickListener(MyActionbarClickListener listener)
+	public void setOnMyActionbarClickListener(MyActionbarClickListener listener)//暴露给调用者的方法，这样调用者便可以将具体实现以匿名内部类的
+	//形式传递进来
 	{
 		this.listener = listener;
 	}
@@ -45,7 +47,7 @@ public class MyActionBar extends RelativeLayout {
 	public MyActionBar(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		// TODO Auto-generated constructor stub
-		//获取在attrs.xml中定义的一系列属性
+		//使用TypedArray,获取在attrs.xml中我们自定义的一系列属性,并将属性赋给我们定义的控件,然后我们便可以从TypedArray中获取到这些属性值
 		TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.MyActionBar);
 		leftTextColor = typedArray.getColor(R.styleable.MyActionBar_leftTextColor, 0);
 		leftBackground = typedArray.getDrawable(R.styleable.MyActionBar_leftBackground);
@@ -77,15 +79,15 @@ public class MyActionBar extends RelativeLayout {
 		tvTitle.setTextSize(titleTextSize);
 		tvTitle.setGravity(Gravity.CENTER);//设置标题居中
 		
-		setBackground(actionBarBackground);
-		
+		setBackground(actionBarBackground);//设置viewGroup(即两个button和一个tittle)的背景颜色
+		//以上我们已经设置好了各个控件的属性，那接下来就需要把这些控件放置到viewGroup中了，即我们需要设置一下他们的布局属性了
 		leftParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);//设置左Button的width和height
 		leftParams.addRule(RelativeLayout.ALIGN_PARENT_LEFT, TRUE);//设置左边button的布局属性，即左对齐
 		addView(leftButton, leftParams);//将左边button加入到viewGroup中
 		rightParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		rightParams.addRule(RelativeLayout.ALIGN_PARENT_RIGHT, TRUE);
 		addView(rightButton, rightParams);
-		titleParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT);
+		titleParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 		titleParams.addRule(RelativeLayout.CENTER_IN_PARENT, TRUE);
 		addView(tvTitle, titleParams);
 		
